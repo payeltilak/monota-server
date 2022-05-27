2db52ceff398e6389cd8676a9eb7053971619d86
unknown <you@example.com>
2022-05-27 21:20:45 +0600

    JWT implemented

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 
@@ -17,12 +18,81 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db('monota').collection('products');
+        const orderCollection = client.db('monota').collection('orders');
+        const userCollection = client.db('monota').collection('users');
         
         app.get('/product', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products)
+        })
+        app.get('/tool/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const product = await productCollection.findOne(query)
+            res.send(product);
+        });
+
+
+        // put user to db
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            console.log('Hit');
+            const filter = { email: email }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options)
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            res.send({ result, token })
+        })
+
+
+
+        // put order in orders collection
+        app.put('/order', async (req, res) => {
+            const order = req.body
+            const filter = {
+                email: order.email,
+                toolId: order.toolId
+            }
+            const query = { _id: ObjectId(order.toolId) }
+            const exist = await orderCollection.findOne(filter)
+            const options = { upsert: true };
+            if (exist) {
+                const updatedDoc = {
+                    $set: {
+                        address: order.address,
+                        phone: order.phone,
+                        quantity: exist.quantity + order.quantity,
+                        price: exist.price + order.price
+                    }
+                }
+                const tool = await productCollection.findOne(query)
+                const updateTool = {
+                    $set: {
+                        quantity: tool.quantity - order.quantity
+                    }
+                }
+                const updatedTool = await productCollection.updateOne(query, updateTool, options)
+                const updatedOrder = await orderCollection.updateOne(filter, updatedDoc, options)
+                return res.send({ updatedOrder, updatedTool })
+            }
+
+            else {
+                const tool = await productCollection.findOne(query)
+                const updateTool = {
+                    $set: {
+                        quantity: tool.quantity - order.quantity
+                    }
+                }
+                const updatedTool = await productCollection.updateOne(query, updateTool, options)
+                const result = await orderCollection.insertOne(order)
+                return res.send({ result, updatedTool })
+            }
         })
     }
     finally {
